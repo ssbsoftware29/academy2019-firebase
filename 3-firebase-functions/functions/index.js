465d7161a146e85c73d48665b465fa86929a4d6c
Samuel <samuelformal@gmail.com>
2019-07-16 21:05:44 -0300

    Aula FIrebase-02

--- 3-firebase-functions/functions/index.js
+++ 3-firebase-functions/functions/index.js
@@ -3,13 +3,44 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const firestore = admin.firestore()
+
 const settings = {
   timestampsInSnapshots: true
 }
+
 firestore.settings(settings)
 
-exports.on_create_user = functions.auth.user().onCreate((user) => {
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello, who are you?");
+ });
+
+ exports.sayHelloWorld = functions.https.onRequest(async (req, res) => {
+  res.status(200).send(`Hello world :D, my name is ${req.query.name}`)
+ });
+
+ //FUnção post
+ exports.print_output = functions.https.onRequest(async (req, res) => {
+   //var id = req.body.id
+   //var title = req.body.title
+   //var author = req.body.author
+
+   //req.body.id
+   //req.body.message
+   //req.body.title
+
+   const { message, id, title } = req.body
+
+   res.status(200).send(`Message received! || Id: ${id} || Title: ${title} || Author: ${author}`)
+ });
+
+ //Função trigger
+
+ exports.create_trigger = functions.auth.user().onCreate((user) => {
   const { uid, email, displayName } = user
 
   return firestore.doc(`users/${uid}`).set({ uid, email, displayName })
-});
+ });
